cd1dced40b078729ad9868143dd9f74bef66866b
Anderson Duboc <aduboc@aduboc-a01.vmware.com>
2019-03-15 19:31:11 -0300

    load test

--- load.sh
+++ load.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-loadtest -n 10000 -c 100 --rps 200 http://ec2-54-89-29-170.compute-1.amazonaws.com/ --quiet &
-loadtest -n 10000 -c 100 --rps 200 http://ec2-54-89-29-170.compute-1.amazonaws.com:9090/api/cat --quiet &
-loadtest -n 10000 -c 100 --rps 200  http://ec2-54-89-29-170.compute-1.amazonaws.com:9090/api/dog --quiet &
+loadtest -n 1000 -c 100 --rps 200 http://ec2-54-89-29-170.compute-1.amazonaws.com/ --quiet &
+loadtest -n 10500 -c 100 --rps 200 http://ec2-54-89-29-170.compute-1.amazonaws.com:9090/api/cat --quiet &
+loadtest -n 20100 -c 100 --rps 200  http://ec2-54-89-29-170.compute-1.amazonaws.com:9090/api/dog --quiet &
 loadtest -n 100 http://ec2-54-89-29-170.compute-1.amazonaws.com/test --quiet &
