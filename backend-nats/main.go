96ef7b551e3dddd684f143801cbaed7989ca4b78
Linux User <axpira@nico.br>
2019-12-23 12:05:47 -0300

    Criacao dos backends nats

--- backend-nats/main.go
+++ backend-nats/main.go
@@ -10,12 +10,6 @@ import (
 	"time"
 
 	"github.com/opentracing/opentracing-go"
-	//metrics "github.com/rcrowley/go-metrics"
-	//wavefront "github.com/wavefronthq/go-metrics-wavefront"
-	//"github.com/wavefronthq/wavefront-opentracing-sdk-go/reporter"
-	//"github.com/wavefronthq/wavefront-opentracing-sdk-go/tracer"
-	//"github.com/wavefronthq/wavefront-sdk-go/application"
-	//"github.com/wavefronthq/wavefront-sdk-go/senders"
 
 	metrics "github.com/rcrowley/go-metrics"
 	"github.com/wavefronthq/go-metrics-wavefront/reporting"
@@ -24,25 +18,30 @@ import (
 
 	"github.com/wavefronthq/wavefront-opentracing-sdk-go/reporter"
 	"github.com/wavefronthq/wavefront-opentracing-sdk-go/tracer"
+
+	nats "github.com/nats-io/nats.go"
+)
+
+var (
+	nc *nats.Conn
 )
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
+
+	var err error
+	nc, err = nats.Connect(os.Getenv("NATS_ENDPOINT"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	// Counter metrics registry
-	//metricRequestDurationDog := metrics.GetOrRegisterTimer("request.dog.duration", metrics.DefaultRegistry)
-	//metricRequestDurationCat := metrics.GetOrRegisterTimer("request.cat.duration", metrics.DefaultRegistry)
 	metricRequestDurationDog := createDurationMetric("dog")
 	metricRequestDurationCat := createDurationMetric("cat")
 
 	//Metrics error
-	//counterErrorDog := metrics.GetOrRegisterCounter("errorDog", metrics.DefaultRegistry)
-	//counterErrorCat := metrics.GetOrRegisterCounter("errorCat", metrics.DefaultRegistry)
 	counterErrorDog := createCounterErrorMetric("dog")
 	counterErrorCat := createCounterErrorMetric("cat")
-
-	//hostTags := map[string]string{
-	//	"source": "petshop",
-	//}
 
 	// configure direct sender
 
@@ -69,9 +68,6 @@ func main() {
 		// handle error
 		fmt.Println(err)
 	}
-	// Use the direct sender to send data
-
-	//app metrics https://github.com/wavefrontHQ/wavefront-opentracing-sdk-go
 
 	appTags := application.New("Petshop", "order")
 
@@ -80,14 +76,6 @@ func main() {
 	tracer := tracer.New(reporter)
 
 	opentracing.InitGlobalTracer(tracer)
-
-	////server := os.Getenv("WAVEFRONT_INSTANCE")
-	////token := os.Getenv("WAVEFRONT_TOKEN")
-	////go wavefront.WavefrontDirect(metrics.DefaultRegistry, 5*time.Second, hostTags, "zoologico", server, token)
-	//// report to a Wavefront proxy
-	//addr, _ := net.ResolveTCPAddr("tcp", os.Getenv(""))
-	//go wavefront.WavefrontProxy(metrics.DefaultRegistry, 5*time.Second, hostTags, "petshop", addr)
-	////go metrics.Log(metrics.DefaultRegistry, 10*time.Second, log.New(os.Stdout, "metrics: ", log.Lmicroseconds))
 
 	fmt.Println("Listen 9090 .....")
 	http.HandleFunc("/api/dog", handler(metricRequestDurationDog, counterErrorDog))
@@ -131,9 +119,13 @@ func handler(durationMetric metrics.Timer, errorMetric metrics.Counter) func(htt
 				errorMetric.Inc(1)
 				w.Header().Set("Content-Type", "text/plain")
 				w.Write([]byte("Erro ao processar"))
+				fmt.Println(nc.Publish("animals_errors", []byte("Erro ao processar")))
 			} else {
 				w.Header().Set("Content-Type", "application/json")
 				json.NewEncoder(w).Encode(response{Animal: animal})
+				str, _ := json.Marshal(response{Animal: animal})
+				fmt.Println(nc.Publish("animals", []byte(str)))
+
 			}
 		})
 	}
