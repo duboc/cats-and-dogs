ac23064a33f76d67ae4be2c53f246b7a5aa8d68e
Anderson Duboc <andersonduboc@gmail.com>
2020-02-24 01:09:25 -0300

    new deploy params

--- backend-nats/deploy.sh
+++ backend-nats/deploy.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 /usr/local/go/bin/go build  
 mv backend-nats /var/apps
+
+#ip-tables for nats with app
+iptables -A INPUT -p tcp --destination-port 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --destination-port 4222 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --destination-port 8222 -m state --state NEW,ESTABLISHED -j ACCEPT
+
+iptables -A OUTPUT  -p tcp --source-port 80 -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT  -p tcp --source-port 4222 -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT  -p tcp --source-port 8222 -m state --state ESTABLISHED -j ACCEPT
+
+#starting service
+systemctl enable backend-nats
+systemctl start backend-nats
